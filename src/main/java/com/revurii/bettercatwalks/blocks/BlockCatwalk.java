a8612349e7d1c1cff575a99e8a6173b30d7641b8
revurii <koukuyou.mi@gmail.com>
2026-03-30 20:43:44 +0800

    move utility methods to new utils class

--- src/main/java/com/revurii/bettercatwalks/blocks/BlockCatwalk.java
+++ src/main/java/com/revurii/bettercatwalks/blocks/BlockCatwalk.java
@@ -1,8 +1,5 @@
 package com.revurii.bettercatwalks.blocks;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +18,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.gtnewhorizon.gtnhlib.client.model.ModelISBRH;
+import com.revurii.bettercatwalks.utils.CatwalkUtils;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -30,9 +28,9 @@ public class BlockCatwalk extends Block {
     private static final float PX = 0.0625f;
 
     private static final AxisAlignedBB[] CATWALK_BOUNDS = {
-        // Railing 1
+        // North Railing
         AxisAlignedBB.getBoundingBox(0, 0.0001, 0, 1, 14 * PX, 2 * PX),
-        // Railing 2
+        // South Railing
         AxisAlignedBB.getBoundingBox(0, 0.0001, 14 * PX, 1, 14 * PX, 16 * PX),
         // Base
         AxisAlignedBB.getBoundingBox(0.0001, 0, 0.0001, 0.9999, 2 * PX, 0.9999) };
@@ -71,13 +69,8 @@ public class BlockCatwalk extends Block {
         // endVec = farthest point the player can reach if not obstructed
 
         // Get the first AABB in the player's line of sight
-        Map.Entry<AxisAlignedBB, MovingObjectPosition> aabbMop = getFirstInterceptedAABBandMOP(
-            CATWALK_BOUNDS,
-            x,
-            y,
-            z,
-            startVec,
-            endVec);
+        Map.Entry<AxisAlignedBB, MovingObjectPosition> aabbMop = CatwalkUtils
+            .getFirstInterceptedAABBandMOP(CATWALK_BOUNDS, x, y, z, startVec, endVec);
         if (aabbMop != null) {
             MovingObjectPosition mop = aabbMop.getValue();
             mop.blockX = x;
@@ -109,42 +102,12 @@ public class BlockCatwalk extends Block {
             start.yCoord + look.yCoord * dist,
             start.zCoord + look.zCoord * dist);
 
-        Map.Entry<AxisAlignedBB, MovingObjectPosition> aabbMop = getFirstInterceptedAABBandMOP(
-            CATWALK_BOUNDS,
-            x,
-            y,
-            z,
-            start,
-            end);
+        Map.Entry<AxisAlignedBB, MovingObjectPosition> aabbMop = CatwalkUtils
+            .getFirstInterceptedAABBandMOP(CATWALK_BOUNDS, x, y, z, start, end);
 
         if (aabbMop != null) return aabbMop.getKey();
         return AxisAlignedBB.getBoundingBox(0, 0, 0, 1, 1, 1);
 
-    }
-
-    // TODO: Prioritize AABB based on face instead of slightly adjusting bounds to prevent flickering at intersections
-    protected Map.Entry<AxisAlignedBB, MovingObjectPosition> getFirstInterceptedAABBandMOP(AxisAlignedBB[] aabbs, int x,
-        int y, int z, Vec3 start, Vec3 end) {
-
-        // Build a HashMap of AABBs and the MOP that the player is looking at
-        HashMap<AxisAlignedBB, MovingObjectPosition> aabbMopMap = new HashMap<>();
-        for (AxisAlignedBB aabb : aabbs) {
-
-            // Add x, y, z to min and max of bounds on each axis to position it to the coordinates of the block
-            AxisAlignedBB currentBb = aabb.copy();
-            currentBb.offset(x, y, z);
-
-            // Get the first point between the start and end vector that is on the bounds of the box
-            MovingObjectPosition hit = currentBb.calculateIntercept(start, end);
-            if (hit != null) aabbMopMap.put(currentBb, hit);
-
-        }
-
-        // Return the first intercepted AABB and MOP
-        if (!aabbMopMap.isEmpty()) return Collections
-            .min(aabbMopMap.entrySet(), Comparator.comparingDouble(o -> o.getValue().hitVec.distanceTo(start)));
-
-        return null;
     }
 
     @Override
@@ -169,11 +132,11 @@ public class BlockCatwalk extends Block {
 
     public static class ItemCatwalk extends ItemBlock {
 
-        Block catwalk;
+        BlockCatwalk catwalk;
 
         public ItemCatwalk(Block block) {
             super(block);
-            this.catwalk = block;
+            this.catwalk = (BlockCatwalk) block;
         }
 
         /**
@@ -183,7 +146,7 @@ public class BlockCatwalk extends Block {
         @SideOnly(Side.CLIENT)
         public boolean func_150936_a(World world, int x, int y, int z, int side, EntityPlayer player, ItemStack stack) {
 
-            Vec3 placeVec = getCatwalkPlacementPosition(player, world, x, y, z, side);
+            Vec3 placeVec = CatwalkUtils.getCatwalkPlacementPosition(player, world, x, y, z, side);
 
             // Check entity collisions for each defined AABB instead of using the superclass method
             for (AxisAlignedBB origBb : CATWALK_BOUNDS) {
@@ -200,7 +163,11 @@ public class BlockCatwalk extends Block {
         public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
             float hitX, float hitY, float hitZ) {
 
-            Vec3 placeVec = getCatwalkPlacementPosition(player, world, x, y, z, side);
+            // onItemUse() is called a second time by ForgeHooks
+            // On the second call, the player's position and look vector is based on their feet instead of the eyes
+            // Use Minecraft.getMinecraft().thePlayer to be consistent
+            Vec3 placeVec = CatwalkUtils
+                .getCatwalkPlacementPosition(Minecraft.getMinecraft().thePlayer, world, x, y, z, side);
 
             // Determine if the catwalk can be placed based on some additional conditions
             if (stack.stackSize == 0) {
@@ -237,59 +204,6 @@ public class BlockCatwalk extends Block {
                     }
         }
 
-        // TODO: Change this to place based on the position of the cursor on the AABB instead
-        protected Vec3 getCatwalkPlacementPosition(EntityPlayer player, World world, int x, int y, int z, int side) {
-
-            // If the player is sneaking or looking at a catwalk, follow normal block placement behavior
-            // Otherwise, the catwalk will be placed based on the cardinal direction that the player is facing
-
-            if (player.isSneaking() || world.getBlock(x, y, z) != this.catwalk) {
-
-                switch (side) {
-                    case 0 -> y--; // BOTTOM
-                    case 1 -> y++; // TOP
-                    case 2 -> z--; // NORTH
-                    case 3 -> z++; // SOUTH
-                    case 4 -> x--; // WEST
-                    case 5 -> x++; // EAST
-                }
-
-            } else {
-
-                // Translate yaw to the value seen in F3 (-180 to 180)
-                float yaw = player.rotationYaw;
-                yaw %= 360;
-
-                if (yaw > 180) {
-                    yaw -= 360;
-                } else if (yaw < -180) {
-                    yaw += 360;
-                }
-
-                if (yaw >= -45 && yaw < 45) {
-                    z++; // SOUTH
-                } else if (yaw >= 45 && yaw < 135) {
-                    x--; // WEST
-                } else if (yaw >= 135 && yaw <= 180 || yaw >= -180 && yaw < -135) {
-                    z--; // NORTH
-                } else if (yaw >= -135 && yaw < -45) {
-                    x++; // EAST
-                }
-
-            }
-
-            return Vec3.createVectorHelper(x, y, z);
-        }
-
-    }
-
-    private static String msg = "";
-
-    private static void log(String s) {
-        if (!msg.equals(s)) {
-            msg = s;
-            System.out.println("LOG: " + msg);
-        }
     }
 
 }
---
+++ src/main/java/com/revurii/bettercatwalks/utils/CatwalkUtils.java
@@ -0,0 +1,105 @@
+package com.revurii.bettercatwalks.utils;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+
+import com.revurii.bettercatwalks.ModBlocks;
+
+public class CatwalkUtils {
+
+    /**
+     * Get the position that a catwalk would be placed in given the position and side of the block the player
+     * right-clicked while holding a catwalk
+     */
+    public static Vec3 getCatwalkPlacementPosition(EntityPlayer player, World world, int x, int y, int z, int side) {
+
+        // If the player is sneaking or looking at a catwalk, follow normal block placement behavior
+        // Otherwise, the catwalk will be placed based on the cardinal direction that the player is facing
+
+        if (player.isSneaking() || world.getBlock(x, y, z) != ModBlocks.CATWALK.get()) {
+
+            switch (side) {
+                case 0 -> y--; // BOTTOM
+                case 1 -> y++; // TOP
+                case 2 -> z--; // NORTH
+                case 3 -> z++; // SOUTH
+                case 4 -> x--; // WEST
+                case 5 -> x++; // EAST
+            }
+
+        } else {
+
+            // Translate yaw to the value seen in F3 (-180 to 180)
+            float yaw = player.rotationYaw;
+            yaw %= 360;
+
+            if (yaw > 180) {
+                yaw -= 360;
+            } else if (yaw < -180) {
+                yaw += 360;
+            }
+
+            if (yaw >= -45 && yaw < 45) {
+                z++; // SOUTH
+            } else if (yaw >= 45 && yaw < 135) {
+                x--; // WEST
+            } else if (yaw >= 135 && yaw <= 180 || yaw >= -180 && yaw < -135) {
+                z--; // NORTH
+            } else if (yaw >= -135 && yaw < -45) {
+                x++; // EAST
+            }
+
+        }
+
+        return Vec3.createVectorHelper(x, y, z);
+    }
+
+    /**
+     * Get the first intercepted AABB between the start and end vector from a list of AABBs positioned at the given
+     * coordinates
+     */
+    public static Map.Entry<AxisAlignedBB, MovingObjectPosition> getFirstInterceptedAABBandMOP(AxisAlignedBB[] aabbs,
+        int x, int y, int z, Vec3 start, Vec3 end) {
+
+        // Build a HashMap of AABBs and the MOP containing the position of the intercept
+        HashMap<AxisAlignedBB, MovingObjectPosition> aabbMopMap = new HashMap<>();
+        for (AxisAlignedBB aabb : aabbs) {
+
+            // Add x, y, z to min and max of bounds on each axis to position it to the coordinates of the block
+            AxisAlignedBB currentBb = aabb.copy();
+            currentBb.offset(x, y, z);
+
+            // Get the first point between the start and end vector that is on the bounds of the box
+            MovingObjectPosition hit = currentBb.calculateIntercept(start, end);
+            if (hit != null) aabbMopMap.put(currentBb, hit);
+
+        }
+
+        // Return the first intercepted AABB and its MOP
+        if (!aabbMopMap.isEmpty()) return Collections
+            .min(aabbMopMap.entrySet(), Comparator.comparingDouble(o -> o.getValue().hitVec.distanceTo(start)));
+
+        return null;
+    }
+
+    private static String msg = "";
+
+    /**
+     * Log a String for testing purposes; will not print the same String more than once
+     */
+    public static void log(String s) {
+        if (!msg.equals(s)) {
+            msg = s;
+            System.out.println("LOG: " + msg);
+        }
+    }
+
+}
